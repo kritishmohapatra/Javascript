1f514e555b6303651d8269442019ebc17330e84d
kritish <kritishmohapatra06norisk@gmail.com>
2025-06-05 20:10:43 +0530

    conversion_with_operations done

---
+++ Basics/conversion_with_operations.js
@@ -0,0 +1,50 @@
+let value=3
+let negvalue=-value 
+console.log(negvalue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**2);
+console.log(2%3);
+
+
+let str1="Kritish"
+let str2=" Mohapatra"
+console.log(str1+str2); //string add
+
+
+console.log("1"+2);
+console.log("1"+"2"+2); //give 122
+console.log(1+2+"2"); //give 32
+
+
+console.log(true);
+console.log(+true);//1
+console.log(+"");//0
+
+
+let num1, num2, num3
+num1=num2=num3=4
+console.table([num1, num2, num3])
+
+
+let c=100
+c++
+console.log(c); //first assign then inc
+++c
+console.log(c);//first inc then assign
+
+
+
+
+
+
+
+
+
+
+
+
+
