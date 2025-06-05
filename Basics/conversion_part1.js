4a59e8bde16e6d57018974de892ae83af1bc4fb3
kritish <kritishmohapatra06norisk@gmail.com>
2025-06-05 19:52:48 +0530

    conversion part 1 completed

---
+++ Basics/conversion_part1.js
@@ -0,0 +1,33 @@
+let score=33
+console.log(typeof(score)) //gives number
+let scoreins="33"
+console.log(typeof(scoreins)) //gives string
+
+
+let scoreinnumber=Number(scoreins) //conversion of string to number
+console.log(typeof(score))  //gives number 
+
+//main point
+let v="33abc"
+let vinnum=Number(v)
+console.log(typeof(vinnum))
+console.log(vinnum) //gives NaN
+// IF NULL THEN GIVES 0
+// IF UNDEFINED THEN Nan
+//IF TRUE 1
+//IF FALSE 0
+//NAN=> NOT A NUMBER
+
+let isloogedin=1
+let boolislogg=Boolean(isloogedin)
+console.log(boolislogg)
+//if string empty then gives false else True
+//1=> Truee 0=> False
+
+let somenum=33
+let strnum=String(somenum)
+console.log(typeof(strnum));
+
+
+
+
