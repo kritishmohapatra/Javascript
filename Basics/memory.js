db84d3bc819f433069dddd681a2628f1e1678bf1
kritish <kritishmohapatra06norisk@gmail.com>
2025-06-06 19:12:30 +0530

    "memory  in js done "

---
+++ Basics/memory.js
@@ -0,0 +1,21 @@
+//stack memory
+//primtive
+
+// heap memory
+//non primitive
+
+let myyoutube="kritish"
+let another=myyoutube
+another="mohapatra"
+
+console.log(another);
+let user={
+    email:"user@gmail.com",
+    upi:"yblkritish"
+}
+let user2=user
+user2.email="mohapatra"
+
+console.log(user);
+console.log(user2);
+
