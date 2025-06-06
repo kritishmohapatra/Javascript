4fa127b1d67c866644e4e762a5ee4f05760b2731
kritish <kritishmohapatra06norisk@gmail.com>
2025-06-06 19:03:24 +0530

    "datatypes summarize done"

---
+++ Basics/datatypes_summary.js
@@ -0,0 +1,30 @@
+//primitive 
+//7 categories 
+//string , number, boolean , null, undefined , symbol, bigint
+const score=100
+const scoval=100.3
+const islog=false
+const outs=null
+let useremail;
+const id =Symbol('123')
+const anid=Symbol("123")
+console.log(id==anid);
+const bignum=3547972189238799n
+
+//Reference type or Non primitive
+// array, objects, functions
+const heros=["shaktiman", "nagraj", "doga"]
+let myobj={
+    name:"kritish",
+    age:15
+}
+const  my=function (){
+    console.log("Kritish");
+    
+
+} // object function 
+// null typeof object
+
+
+//js dynamically type
+
