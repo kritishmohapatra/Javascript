d3a01cfa3ba25e94ee22072207d69c833809b276
kritish <kritishmohapatra06norisk@gmail.com>
2025-06-07 15:05:36 +0530

    "numbers and math done"

---
+++ Basics/number_and_maths.js
@@ -0,0 +1,63 @@
+const score=400
+console.log(score);
+//give onlt the value
+
+const balance =new Number(100)
+console.log(balance);
+// number:100
+
+console.log(balance.toString().length);
+//num to string and length
+
+console.log(balance.toFixed(2));
+//decimal precision 
+
+const other=23.8966
+console.log(other.toPrecision(3));
+//precision
+
+const hundreds=100000
+console.log(hundreds.toLocaleString("en-IN"));
+
+
+console.log(Number.MIN_VALUE);
+console.log(Number.MAX_VALUE);
+
+
+//maths libraray
+console.log(Math);
+//it is object 
+
+console.log(Math.abs(-4));
+//gives absolute value
+
+console.log(Math.round(4.5));
+console.log(Math.ceil(4.6));
+console.log(Math.floor(4.6));
+console.log(Math.max(4, 9, 10, 45));
+
+// math random 
+console.log((Math.random()*10)+1);
+
+const min =10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1) +min))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
