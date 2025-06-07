c1a3dd0ce0dea00b1a535832be1e29a86db581ff
kritish <kritishmohapatra06norisk@gmail.com>
2025-06-07 14:47:34 +0530

    "string in js completed"

---
+++ Basics/string.js
@@ -0,0 +1,61 @@
+console.log("Kritis "+"Mohapatra")
+const name="Kritish"
+const repo=13
+console.log(name+repo);
+
+console.log(`Hello my name is ${name} and rep count is ${repo}`)
+const game =new String("Kritis-h")
+//string is a object not an array
+//indexing start form index 0
+
+console.log(game[0]);
+//accesing  elements
+
+console.log(game.length);
+//gives the length
+
+console.log(game.toUpperCase());
+//uppercase conversion
+
+console.log(game.charAt(2));
+//chracter at index 
+
+console.log(game.indexOf("s"));
+//charcter at which index 
+
+
+const newst=game.substring(0, 5)
+console.log(newst);
+//string slicing
+
+
+const anothers= game.slice(-7,4)
+console.log(anothers);
+//negative starts from -num 
+
+
+const newstr=" kritish    "
+console.log(newstr);
+console.log(newstr.trim())
+//trim 
+
+const url="https://kritish.com/kritish%20"
+
+console.log(url.replace("%20", "-"));
+// replace
+
+console.log(url.includes("kritish"));
+// give true if includes
+
+
+console.log(game.split("-"));
+//give list by spilting at -
+
+
+
+
+
+
+
+
+
