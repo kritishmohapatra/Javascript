8b95d1930feccadd00b30662b9e452eb4199c8e1
kritish <kritishmohapatra06norisk@gmail.com>
2025-06-12 16:36:10 +0530

    "objects part_3 completed"

---
+++ Basics part 2/objects_part3.js
@@ -0,0 +1,27 @@
+//Destructure
+const course={
+    name:"jshindi",
+    price:"150",
+    inst:"hitseh sir"
+}
+
+//course.inst 
+const {inst: ins}=course
+console.log(ins);
+
+
+// const navbar=({company}){
+
+// }
+// navbar(company="hitesh")
+
+//concept of APIS
+//JSON STRUCT 
+
+// {
+//     "name":"kritish",
+//     "price":"150",
+    
+
+    
+// }
