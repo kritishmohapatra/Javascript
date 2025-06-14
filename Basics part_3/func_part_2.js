b884b845a0b18959e0bd2dd280a9460d30b53fe1
kritish <kritishmohapatra06norisk@gmail.com>
2025-06-14 12:35:11 +0530

    "function part 2 done"

---
+++ Basics part_3/func_part_2.js
@@ -0,0 +1,28 @@
+function calculateprice(val1, val2,...num1){ // rest operator
+    return num1
+
+}
+console.log(calculateprice(200, 400, 500, 2000)); // gives array
+//val1=200
+//val2=400
+
+const user={
+    username:"kritish",
+    price:150
+}
+// passing objects
+function handle(anyobj){
+    console.log(`username is  ${anyobj.username} and price is ${anyobj.price}`);
+    
+    
+}
+handle(user)
+
+// passing array
+const myarr=[400, 1600, 800]
+function returnsecond(arr){
+    return arr[2]
+
+}
+console.log(returnsecond(myarr));
+ 
