7a171cf7c2c729fa4a17e82521a8037ccc85035f
kritish <kritishmohapatra06norisk@gmail.com>
2025-06-19 01:25:55 +0530

    "while loops completed"

---
+++ Iterations/part_2.js
@@ -0,0 +1,26 @@
+i=1
+while (i<=10) {
+    console.log(`Number is ${i}`);
+    i+=1
+    
+    
+}
+
+let myarr=[1, 2, 3]
+i=0
+while (i<myarr.length) {
+    console.log(`VALUE IS ${myarr[i]}`);
+    i+=1
+    
+    
+}
+
+
+// do while
+let score=11
+do {
+    console.log(`Score is ${score}`);
+    score+=1
+    
+    
+} while (score<=10);
