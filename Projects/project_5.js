da24d7c791da00d5a0d1cf16b1af21931b841ca9
kritish <kritishmohapatra06norisk@gmail.com>
2025-06-30 18:24:52 +0530

    "project 5 completed"

---
+++ Projects/project_5.js
@@ -0,0 +1,19 @@
+const ins = document.getElementById('insert');
+window.addEventListener('keydown', (e) => {
+  ins.innerHTML = `
+  <div class='color'>
+    <table>
+    <tr>
+      <th>Key</th>
+      <th>Keycode</th> 
+      <th>Code</th>
+    </tr>
+    <tr>
+      <td>${e.key === ' ' ? 'Space' : e.key}</td>
+      <td>${e.keyCode}</td> 
+      <td>${e.code}</td>
+    </tr>
+    
+  </table>
+    </div>`;
+});
